b3bbd0c9afcb01105f284ef9cedf1b9d5cba56a6
cfchung <sweetflute@gmail.com>
2014-11-07 13:39:46 -0800

    Adding sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,18 @@ $(document).on("click", "#menulist li a", function(ev){
   return false;
 })
 
+$(document).on("click", "#subcategory li a", function(ev){
+  ev.preventDefault();
+    ev.stopPropagation();
+  $.ajax({
+   url: $(this).attr('href'),
+   cache: false,
+   dataType: "html",
+   success: function(data) {
+       $("#research_area").html(data);
+   }
+  });
+  return false;
+})
+
+
