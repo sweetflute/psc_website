578ad2e0d11db509a700bcccd862e60d6049d6b5
A. Conrad Nied <anied@cs.washington.edu>
2015-02-20 14:21:48 -0800

    Handle page changes better

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,8 @@ $(function() {$("#content").load("frontpage.html");});
 /**
  * Instead of changing the actual page, the website loads subpages into the index page.
  */
-$(document).on("click", "#menulist li a", function(ev){
-  var pagename = $(this).attr('href');
-  var formattedpagename = pagename.substring(0, pagename.lastIndexOf("."));
-
-  // This fakes out the page name, but this doesn't help in retrieving pages
-  // window.history.pushState('page2', 'Title', formattedpagename);
-
-  window.location.hash = formattedpagename;
-
-  ev.preventDefault();
-  ev.stopPropagation();
-  changePage(pagename, "#content");
-
-  return false;
-})
+$(document).on("click", "a.navbar-brand", interceptPageChange);
+$(document).on("click", "#menulist li a", interceptPageChange);
 
 $(document).on("click", "#subcategory li a", function(ev){
 
@@ -26,15 +13,45 @@ $(document).on("click", "#subcategory li a", function(ev){
   changePage(pagename, "#research_area");
 
   return false;
-})
+});
 
 window.onload = function () {
   var pagename = window.location.hash;
   if(pagename.length > 0) {
     pagename = pagename.substring(1) + ".html";
-    console.log(pagename);
   }
   changePage(pagename, "#contents")
+}
+
+// $(window).on('hashchange', function(event) {
+//   // console.log("IFRAME HASH CHANGED");
+//   // console.log(event);
+
+//   pagename = window.location.hash;
+//   if(pagename == "")
+//     pagename = "frontpage.html";
+//   else
+//     pagename = pagename + ".html";
+//   changePage(pagename, "#content");
+// });
+
+
+function interceptPageChange(event) {
+  var pagename = $(this).attr('href');
+  var formattedpagename = pagename.substring(0, pagename.lastIndexOf("."));
+  if(formattedpagename == "frontpage") formattedpagename = "";
+  // console.log(pagename);
+
+  // This fakes out the page name, but this doesn't help in retrieving pages
+  // window.history.pushState('page2', 'Title', formattedpagename);
+
+  window.location.hash = formattedpagename;
+
+  event.preventDefault();
+  event.stopPropagation();
+  changePage(pagename, "#content");
+
+  return false;
 }
 
 function changePage(pagename, area) {
